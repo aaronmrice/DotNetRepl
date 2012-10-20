f1bd493de927389620b206c02f6362aa66c75201
Gobiner <aaron.rice.17+github@gmail.com>
2012-10-20 12:24:28 -0700

    Added command line client for the secure REPL process.

---
+++ Gobiner.SecureRepl.CommandLine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gobiner.SecureRepl.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = new Random().Next(10000, 20000);
+            Process.Start("Gobiner.SecureRepl.exe", port.ToString());
+            Thread.Sleep(500);
+            var wcfClient = new Wcf.ReplClient("BasicHttpBinding_IRepl", "http://localhost:" + port + "/Repl");
+
+            var input = "";
+
+            while(true)
+            {
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    break;
+
+                Console.WriteLine(wcfClient.Execute(input));
+            }
+
+            wcfClient.Kill();
+        }
+    }
+}
