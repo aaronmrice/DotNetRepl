e324f2aa870ce1694c6b90826a6e07fbd852159d
Gobiner <aaron.rice.17+github@gmail.com>
2012-10-25 11:02:21 -0700

    Added xUnit tests to confirm the security of the REPL.

---
+++ Gobiner.SecureRepl.Tests/CantWriteFiles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Gobiner.SecureRepl.Tests
+{
+    public class CantWriteFiles : IDisposable
+    {
+        public CantWriteFiles()
+        {
+            if (File.Exists("c:\\test.txt"))
+                File.Delete("c:\\test.txt");
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var repl = new Repl();
+            repl.Execute(@"System.IO.File.WriteAllText(@""c:\test.txt"", ""test"");");
+            Assert.False(File.Exists("c:\\test.txt"));
+        }
+
+        public void Dispose()
+        {
+            if (File.Exists("c:\\test.txt"))
+                File.Delete("c:\\test.txt");
+        }
+    }
+}
---
+++ Gobiner.SecureRepl.Tests/SpinloopExits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Gobiner.SecureRepl.Tests
+{
+    public class SpinloopExits
+    {
+        [Fact(Timeout = 10000)]
+        public void Test()
+        {
+            var repl = new Repl();
+            repl.Execute(@"while(true);");
+            Assert.True(true);
+        }
+    }
+}
