fdf66d65ad9c124e828e40ffd0184bd76b320acf
Gobiner <aaron.rice.17+github@gmail.com>
2012-10-25 20:30:52 -0700

    Restructed tests.

---
+++ Gobiner.SecureRepl.Tests/ReplTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Gobiner.SecureRepl.Tests
+{
+    public class ReplTests
+    {
+        [Fact(Timeout = 10000)]
+        public void ProtectedSpinloopReturns()
+        {
+            var repl = new Repl();
+            repl.Execute(@"try { while(true); } finally { while(true); }");
+            Assert.True(true);
+        }
+
+        [Fact(Timeout = 10000)]
+        public void SpinloopReturns()
+        {
+            var repl = new Repl();
+            repl.Execute(@"while(true);");
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void CantWriteFile()
+        {
+            if (File.Exists("c:\\test.txt"))
+                File.Delete("c:\\test.txt");
+
+            var repl = new Repl();
+            repl.Execute(@"System.IO.File.WriteAllText(@""c:\test.txt"", ""test"");");
+            Assert.False(File.Exists("c:\\test.txt"));
+
+            if (File.Exists("c:\\test.txt"))
+                File.Delete("c:\\test.txt");
+        }
+
+    }
+}
